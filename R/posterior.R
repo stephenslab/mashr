9211c522f83c128053d265066cfe7b0458e5413b
stephens999 <stephens999@gmail.com>
2016-12-17 08:33:07 -0600

    simplified posterior computations

--- R/posterior.R
+++ R/posterior.R
@@ -24,48 +24,38 @@ posterior_mean <- function(bhat, Vinv, U1){
 #' @param Bhat J by R matrix of estimates
 #' @param Shat J x R matrix of standard errors
 #' @param Ulist list of P prior covariance matrices
-#' @return post.means JxPxR array of posterior means, correspodning to the posterior mean ##' for the Jth individual in the Kth compoenent across all R tissues
-#' @return post.covs JxPxR array of posterior vars, correspodning to the posterior vars ##' for the Jth individual in the Kth compoenent across all R tissues
-#' @return post.ups JxPxR array of posterior tailprobs, corresponding to the marginal
-#' @return upper tail probability for the Jth individual in the Pth compoenent across all R
-#' @return post.downs JxKxR array of posterior tailprobs, corresponding to the marginal
-#' @return lower  tail probability for the Jth individual in the Pth component across all R
-#' @return post.nulls JxKxR array of posterior nullprobs, corresponding to the marginal
-#' @return "null probability"" for the Jth individual in the Pth component across all R
+#' @return post_means JxPxR array of posterior means
+#' @return post_vars JxPxR array of posterior (marginal) variances
+#' @return post_pos JxPxR array of posterior (marginal) probability of being positive
+#' @return post_neg JxPxR array of posterior (marginal) probability of being negative
+#' @return post_zero JxPxR array of posterior (marginal) probability of being zero
 #' @export
 
 compute_posterior_arrays=function(Bhat,Shat,Ulist){
   R=ncol(Bhat)
   J=nrow(Bhat)
   P=length(Ulist)
-  post_means=array(NA,dim=c(J,P,R))
-  post_vars=array(NA,dim=c(J,P,R))
-  post_ups=array(NA,dim=c(J,P,R))
-  post_nulls=array(NA,dim=c(J,P,R))
-  post_downs=array(NA,dim=c(J,P,R))
+  post_mean=array(NA,dim=c(J,P,R))
+  post_var=array(NA,dim=c(J,P,R))
+  post_pos=array(NA,dim=c(J,P,R))
+  post_zero=array(NA,dim=c(J,P,R))
+  post_neg=array(NA,dim=c(J,P,R))
 
   for(j in 1:J){
     bhat=as.vector(t(Bhat[j,]))##turn i into a R x 1 vector
-    Vhat=diag(Shat[j,])^2
-    Vinv <- solve(Vhat)
+    V=diag(Shat[j,])^2
+    Vinv <- solve(V)
     for(p in 1:P){
       U1 <- posterior_cov(Vinv, Ulist[[p]])
       mu1 <- as.array(posterior_mean(bhat, Vinv, U1))
-      post_means[j,p,]=mu1
-      post_vars[j,p,]=as.array(diag(U1))
-      for(r in 1:R){##` marginal calculation for each tissue in each component
-        if(post_vars[j,p,r]==0){###if the covariance matrix has entry 0, then p(null=1)
-          post_ups[j,p,r]=0#need to figure out how to make the list have individual entries
-          post_downs[j,p,r]=0
-          post_nulls[j,p,r]=1}
-        else{
-          post_ups[j,p,r]=pnorm(0,mean=mu1[r],sd=sqrt(diag(U1)[r]),lower.tail=F)
-          post_downs[j,p,r]=pnorm(0,mean=mu1[r],sd=sqrt(diag(U1)[r]),lower.tail=T)
-          post_nulls[j,p,r]=0}
-      }
+      post_mean[j,p,]= mu1
+      post_var[j,p,]= diag(U1)
+      post_pos[j,p,] = ifelse(post_var[j,p,]==0,0,pnorm(0,mean=mu1,sqrt(diag(U1)),lower.tail=F))
+      post_neg[j,p,] = ifelse(post_var[j,p,]==0,0,pnorm(0,mean=mu1,sqrt(diag(U1)),lower.tail=T))
+      post_zero[j,p,] = ifelse(post_var[j,p,]==0,1,0)
     }
   }
-  return(list(post_means=post_means,post_nulls=post_nulls,
-              post_vars=post_vars,post_ups=post_ups,post_downs=post_downs))
+  return(list(post_mean=post_mean,post_zero=post_zero,
+              post_var=post_var,post_pos=post_pos,post_neg=post_neg))
 }
 
--- tests/testthat/test_posterior.R
+++ tests/testthat/test_posterior.R
@@ -4,12 +4,13 @@ test_that("array of posteriors looks right", {
   Ulist = compute_covs_singletons(Bhat)
   U1 = posterior_cov(diag(3),Ulist[[1]])
   mu1 = posterior_mean(Bhat[1,],diag(3),U1)
-  post_arrays = compute_posterior_arrays(Bhat, Shat, Ulist)
-  expect_equal(post_arrays$post_means[1,1,],as.vector(mu1))
+  post_array = compute_posterior_arrays(Bhat, Shat, Ulist)
+  expect_equal(post_array$post_mean[1,1,],as.vector(mu1))
 
   U1 = posterior_cov(diag(0.5^2,3),Ulist[[2]])
   mu1 = posterior_mean(Bhat[2,],diag(0.5^2,3),U1)
-  expect_equal(post_arrays$post_means[2,2,],as.vector(mu1))
+  expect_equal(post_array$post_mean[2,2,],as.vector(mu1))
 
+  expect_equal(post_array$post_pos + post_array$post_neg + post_array$post_zero, array(1,dim=c(2,3,3)))
 }
 )
