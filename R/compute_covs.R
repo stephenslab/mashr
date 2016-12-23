58bc302d6cb76c7fbaefac49f5a72b5aef65fcf7
stephens999 <stephens999@gmail.com>
2016-12-23 12:39:57 +0000

    add function to compute Ulist

--- R/compute_covs.R
+++ R/compute_covs.R
@@ -1,8 +1,29 @@
+#' Compute a list of covariance matrices using supplied functions, and put them all together
+#' @param Bhat an N by R matrix of data to be passed to functions fns
+#' @param fns a vector of functions to be applied to Bhat to compute covariance matrices
+#' @param Ulist optionally, an existing list of matrices to which the newly computed matrices will be added
+#' @return a list of covariance matrices
+#' export
+compute_Ulist = function(Bhat,
+                        fns=c(compute_covs_identity,compute_covs_singletons,compute_covs_allones),
+                        Ulist = NULL){
+  c(Ulist,unlist(lapply(fns,function(f){f(Bhat)}),recursive=FALSE))
+}
+
+
+#' Compute an identity matrix
+#' @param Bhat an N by R matrix of data
+#' @return a list with one entry, an R by R identity matrix
+#' export
 compute_covs_identity = function(Bhat){
   R = ncol(Bhat)
   return(list(diag(R)))
 }
 
+#' Compute all R singleton matrices corresponding to condition-specific effects
+#' @param Bhat an N by R matrix of data
+#' @return a list with R entries, the rth entry being an R by R matrix with all 0s except the (r,r) element is 1
+#' export
 compute_covs_singletons = function(Bhat){
   R = ncol(Bhat)
   nullmatrix = matrix(0,nrow=R,ncol=R)
@@ -15,6 +36,10 @@ compute_covs_singletons = function(Bhat){
   return(Ulist)
 }
 
+#' Compute an R by R matrix of all 1s
+#' @param Bhat an N by R matrix of data
+#' @return a list with 1 entry, the R by R matrix of all 1s
+#' export
 compute_covs_allones = function(Bhat){
   R = ncol(Bhat)
   onematrix = matrix(1,nrow=R,ncol=R)
--- tests/testthat/test_cov.R
+++ tests/testthat/test_cov.R
@@ -6,5 +6,7 @@ test_that("prior covariance computations look right", {
   expect_equal(Ulist, list(cbind(c(1,1),c(1,1))))
   Ulist = compute_covs_identity(Bhat)
   expect_equal(Ulist, list(cbind(c(1,0),c(0,1))))
+  Ulist = compute_Ulist(Bhat, c(compute_covs_allones), Ulist)
+  expect_equal(Ulist, list(cbind(c(1,0),c(0,1)), cbind(c(1,1),c(1,1))))
 }
 )
