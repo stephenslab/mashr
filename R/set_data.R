78e673e9547d72f0a2cc4b2ddcea330fbb0dc645
stephens999 <stephens999@gmail.com>
2016-12-24 17:54:36 +0000

    update to introduce data; name Ulist elements

---
+++ R/set_data.R
@@ -0,0 +1,15 @@
+#' Create a data object for mash analysis
+#' @param Bhat an N by R matrix of observed estimates
+#' @param Shat an N by R matrix of corresponding standard errors
+#' @return a data object for passing into mash functions
+#' @export
+set_mash_data = function(Bhat,Shat){
+  return(list(Bhat=Bhat, Shat=Shat))
+}
+
+
+n_conditions = function(data){
+  ncol(data$Bhat)
+}
+
+n_effects = function(data){nrow(data$Bhat)}
