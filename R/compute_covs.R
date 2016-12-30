ba791d9db6e83f3e774df633a7f89e2e60f418e0
stephens999 <stephens999@gmail.com>
2016-12-30 21:04:19 +0000

    update interface so that names of covariances are specified in call to compute_cov

--- R/compute_covs.R
+++ R/compute_covs.R
@@ -1,38 +1,53 @@
 #' Compute a list of covariance matrices using supplied functions, and put them all together
 #' @param data a mash data object, eg as created by \code{set_mash_data}
-#' @param fns a vector of functions to be applied to Bhat to compute covariance matrices
+#' @param methods a named list defining the methods (functions and arguments) to be used
 #' @param Ulist optionally, an existing list of matrices to which the newly computed matrices will be added
 #' @return a list of covariance matrices
+#' @details Each element of methods may be either a character string which names a pre-defined method (see \code{cov_methods}) or a list with elements fn and args, specifying the function to call, and any additional arguments
+#' @examples data = set_mash_data(Bhat = cbind(c(1,2),c(3,4)), Shat = cbind(c(1,1),c(1,1)))
+#'  compute_cov(data,"id")
+#'  compute_cov(data,"singletons")
+#'  compute_cov(data,c("id","sing")) # can use partial matching of names
+#' compute_cov(data,list("sing","id",eg = list(fn = cov_simplehet, args= list(corr=c(0.1,0.2)))))
 #' @export
-compute_Ulist = function(data,
-                        fns=c(cov_identity,cov_singletons,cov_allones,cov_allzeros),
+compute_cov = function(data,
+                        cov_methods,
                         Ulist = NULL){
-  if(length(fns)==1){fns = c(fns)}
-  names(fns)=NULL #avoid any names that happen to be passed in being passed on to the output
-  c(Ulist,unlist(lapply(fns,function(f){f(data)}),recursive=FALSE))
+  res = list()
+  for(i in 1:length(cov_methods)){
+    if(is.character(cov_methods[[i]])){
+      name = match.arg(cov_methods[[i]], names(cov_methods()))
+      cov_method = cov_methods()[[ name ]]
+    } else {
+      cov_method = cov_methods[[i]]
+      name = names(cov_methods)[i]
+    }
+    res[[i]] =  do.call(cov_method$fn, args = modifyList(list(data=data), as.list(cov_method$args) ))
+
+    if(is.list(res[[i]])){ #append _i to names if function retuns multiple matrices
+      names(res[[i]]) = paste0(name, "_", 1:length(res[[i]]) )
+    } else {
+      res[[i]] = list(res[[i]])
+      names(res[[i]]) = name
+    }
+  }
+  res = unlist(res,recursive=FALSE)
+  if(!is.null(Ulist)){res = c(Ulist,res)}
+  return(res)
 }
 
-#maps string names to functions used to compute different types of covariance matrix
-Umap = function(){
-  list("allzeros"= cov_allzeros,
-       "id" = cov_identity,
-       "singletons" = cov_singletons,
-       "allones" = cov_allones)
+
+
+#returns a named list of defaults for covariance function calculation
+cov_methods = function(){
+  list("all_zeros"= list(fn = cov_all_zeros, args = NULL),
+        "id" = list(fn = cov_identity, args= NULL),
+        "singletons" = list(fn = cov_singletons, args=NULL),
+        "all_ones" = list(fn = cov_all_ones, args = NULL),
+        "simple_het" = list(fn = cov_simple_het, args = list(corr=c(0.25,0.5,0.75)))
+  )
 }
 
-#' Compute a list of covariance matrices (with methods indicated by names)
-#' @param data a mash data object, eg as created by \code{set_mash_data}
-#' @param Utypes a vector of strings that indicate which covariance matrices to compute. See \code{Umap}.
-#' @param Ulist optionally, an existing list of matrices to which the newly computed matrices will be added
-#' @return a list of covariance matrices
-#' @export
-compute_Ulist_byname = function(data,
-           Utypes=c("id","singletons","allones","allzeros"),
-           Ulist = NULL){
-  Utypes = match.arg(Utypes,several.ok=TRUE)
-  Ufns = Umap()[Utypes] # map the names in Utypes to functions
-  compute_Ulist(data, Ufns, Ulist)
-}
 
 #' Compute an identity matrix
 #' @param data a mash data object, eg as created by \code{set_mash_data}
@@ -40,7 +55,7 @@ compute_Ulist_byname = function(data,
 #' @export
 cov_identity = function(data){
   R = n_conditions(data)
-  return(list(id=diag(R)))
+  diag(R)
 }
 
 #' Compute all R singleton matrices corresponding to condition-specific effects
@@ -56,7 +71,6 @@ cov_singletons = function(data){
     Ulist[[r]] = nullmatrix
     Ulist[[r]][r,r] = 1
   }
-  names(Ulist) <- paste0("singleton_", 1:R)
   return(Ulist)
 }
 
@@ -64,26 +78,39 @@ cov_singletons = function(data){
 #' @param data a mash data object, eg as created by \code{set_mash_data}
 #' @return a list with 1 entry, the R by R matrix of all 1s
 #' @export
-cov_allones = function(data){
+cov_all_ones = function(data){
   R = n_conditions(data)
-  onematrix = matrix(1,nrow=R,ncol=R)
-  return(list(allones = onematrix))
+  matrix(1,nrow=R,ncol=R)
 }
 
 #' Compute an R by R matrix of all 0s
 #' @param data a mash data object, eg as created by \code{set_mash_data}
 #' @return a list with 1 entry, the R by R matrix of all 0s
 #' @export
-cov_allzeros = function(data){
+cov_all_zeros = function(data){
   R = ncol(data$Bhat)
-  zeromatrix = matrix(0,nrow=R,ncol=R)
-  return(list(allzeros = zeromatrix))
+  matrix(0,nrow=R,ncol=R)
+}
+
+#' For each element of corr compute a matrix with diagonal element 1 and off-diagonal element corr
+#' @param data a mash data object, eg as created by \code{set_mash_data}
+#' @return a list of matrices
+#' @export
+cov_simple_het = function(data, corr){
+  R = n_effects(data)
+  simplehet=list()
+  for(i in 1:length(corr)){
+    if(corr[i]>1 | corr[i]<(-1)){stop("corr must be between -1 and 1")}
+    simplehet[[i]] = matrix(corr[i],nrow=R,ncol=R)
+    diag(simplehet[[i]]) <- 1
+  }
+  return(simplehet)
 }
 
 #' Scale each covariance matrix in list Ulist by a scalar in vector grid
 #' @param Ulist a list of matrices
 #' @param grid a vector of scaling factors
-#' @return a list with length length(Ulist) \times length(grid), with values grid[i]\times Ulist[[j]]
+#' @return a list with length length(Ulist)*length(grid), with values grid[i]*Ulist[[j]]
 #' @export
 scale_cov = function(Ulist, grid){
   orig_names = names(Ulist)
--- tests/testthat/test_cov.R
+++ tests/testthat/test_cov.R
@@ -2,31 +2,44 @@ test_that("prior covariance computations look right", {
   Bhat = rbind(c(1,2),c(2,4))
   Shat = rbind(c(1,1),c(1,1))
   data = set_mash_data(Bhat,Shat)
-  Ulist = cov_singletons(data)
-  expect_equal(Ulist, list(singleton_1=cbind(c(1,0),c(0,0)), singleton_2=cbind(c(0,0),c(0,1))))
 
-  Ulist = cov_allones(data)
-  expect_equal(Ulist, list(allones=cbind(c(1,1),c(1,1))))
+  Ulist = cov_singletons(data)
+  expect_equal(Ulist, list(cbind(c(1,0),c(0,0)), cbind(c(0,0),c(0,1))))
+
+  Ulist = cov_all_ones(data)
+  expect_equal(Ulist, cbind(c(1,1),c(1,1)))
 
   Ulist = cov_identity(data)
-  expect_equal(Ulist, list(id=cbind(c(1,0),c(0,1))))
+  expect_equal(Ulist, cbind(c(1,0),c(0,1)))
 
-  Ulist = compute_Ulist_byname(data, "allones", Ulist)
-  expect_equal(Ulist, list(id = cbind(c(1,0),c(0,1)), allones=cbind(c(1,1),c(1,1))))
+  Ulist = compute_cov(data, list(id2 = list(fn=cov_identity)))
+  expect_equal(Ulist, list(id2=cbind(c(1,0),c(0,1))))
+
+  Ulist = compute_cov(data, "id")
+  expect_equal(Ulist, list(id=cbind(c(1,0),c(0,1))))
+  Ulist = compute_cov(data, "all_ones", Ulist)
+  expect_equal(Ulist, list(id = cbind(c(1,0),c(0,1)), all_ones=cbind(c(1,1),c(1,1))))
 
   Ulist = scale_cov(Ulist, c(1,2))
-  expect_equal(Ulist, list(id.1 = cbind(c(1,0),c(0,1)), allones.1 = cbind(c(1,1),c(1,1)),
-               id.2 = cbind(c(2,0),c(0,2)), allones.2 = cbind(c(2,2),c(2,2))))
+  expect_equal(Ulist, list(id.1 = cbind(c(1,0),c(0,1)), all_ones.1 = cbind(c(1,1),c(1,1)),
+               id.2 = cbind(c(2,0),c(0,2)), all_ones.2 = cbind(c(2,2),c(2,2))))
 
   # test naming of scaled Ulist
-  Ulist = cov_singletons(data)
-  Ulist = compute_Ulist(data, cov_allzeros, Ulist)
+  Ulist = compute_cov(data,"sing")
+  Ulist = compute_cov(data, "all_zeros", Ulist)
   Ulist = scale_cov(Ulist, c(3,4))
-  expect_equal(Ulist, list(singleton_1.1 = cbind(c(3,0),c(0,0)),
-                           singleton_2.1 = cbind(c(0,0),c(0,3)),
-                           allzeros.1 = cbind(c(0,0),c(0,0)),
-                           singleton_1.2 = cbind(c(4,0),c(0,0)),
-                           singleton_2.2 = cbind(c(0,0),c(0,4)),
-                           allzeros.2 = cbind(c(0,0),c(0,0))) )
+  expect_equal(Ulist, list(singletons_1.1 = cbind(c(3,0),c(0,0)),
+                           singletons_2.1 = cbind(c(0,0),c(0,3)),
+                           all_zeros.1 = cbind(c(0,0),c(0,0)),
+                           singletons_1.2 = cbind(c(4,0),c(0,0)),
+                           singletons_2.2 = cbind(c(0,0),c(0,4)),
+                           all_zeros.2 = cbind(c(0,0),c(0,0))) )
+
+  # test matching of arguments
+  Ulist = compute_cov(data, c("all_z","all_o"))
+  expect_equal(Ulist, list(all_zeros = cbind(c(0,0),c(0,0)), all_ones = cbind(c(1,1),c(1,1))))
+
+
+
 }
 )
