6d7985a2235a2af9ddfec0dcd411a1e1ee4d5086
stephens999 <stephens999@gmail.com>
2017-01-31 09:36:10 -0600

    update to pass prior etc to optmethod

--- R/mash.R
+++ R/mash.R
@@ -11,19 +11,29 @@
 #' @param gridmult scalar indicating factor by which adjacent grid values should differ; close to 1 for fine grid
 #' @param grid vector of grid values to use (scaling factors omega in paper)
 #' @param prior indicates what penalty to use on the likelihood
+#' @param optmethod name of optimization method to use
 #' @export
-mash = function(Bhat,Shat, cov_methods = c("identity","singletons"), gridmult= sqrt(2), grid = NULL, prior="nullbiased"){
+mash = function(Bhat,Shat,
+                cov_methods = c("identity","singletons","all_ones","simple_het"),
+                usepointmass=TRUE,
+                gridmult= sqrt(2),
+                grid = NULL,
+                prior="nullbiased",
+                optmethod = c("mixIP")){
   data = set_mash_data(Bhat,Shat)
+  optmethod = match.arg(optmethod)
   if(missing(grid)){grid = autoselect_grid(data,gridmult)}
   #filtered_data = filter_mash_data(data) extract top Z scores
 
   g = add_to_g(data,
                cov_methods,
                grid)
-  g = add_to_g(data, "null", 1, g) # add null to g
+  if(usepointmass){
+    g = add_to_g(data, "null", 1, g) # add null to g
+  }
 
   lik_matrix = calc_relative_lik_matrix(data, g$Ulist)
-  g_opt=optimize_g(g, lik_matrix, prior="nullbiased", optmethod="mixIP")
+  g_opt=optimize_g(g, lik_matrix, prior=prior, optmethod=optmethod)
 
   posterior_weights = compute_posterior_weights(get_mixprob(g_opt), lik_matrix)
   posterior_matrices = compute_posterior_matrices(data, g_opt, posterior_weights)
