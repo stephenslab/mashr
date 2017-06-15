8cbc88e53951aa3f22a3fb4b609ac1a51bbe085a
stephens999 <stephens999@gmail.com>
2017-06-15 12:00:44 -0500

    add likelihood calculations when all observations have same covariances

---
+++ R/likelihoods_common_cov.R
@@ -0,0 +1,14 @@
+#' @title calc_lik_matrix_common_cov
+#' @description computes matrix of likelihoods for each of J rows of Bhat for each of P prior covariances; special case when standard errors and variances are all same across j;
+#' @param data a mash data object, eg as created by \code{set_mash_data}
+#' @param Ulist list of prior covariance matrices
+#' @param log if true computes log-likelihood
+#' @return J x P vector of multivariate normal likelihoods, p(bhat | Ulist[p], V), where V is same for each bhat
+#' @details Compared with \code{calc_lik_matrix} this function exploits fact that observations are iid in this case,
+#' so the inverse covariance matrices only need to be done once, reducing computation to R^3 + JR^2 instead of JR^3
+#' @export
+calc_lik_matrix_common_cov = function(data, Ulist, log=FALSE){
+  V = mashr:::get_cov(data,1) # all covariances are same
+  t(plyr::laply(Ulist, function(U){dmvnorm(x=data$Bhat,sigma=V+U,log=log)}))
+}
+
---
+++ tests/testthat/test_likelihood_common_cov.R
@@ -0,0 +1,10 @@
+test_that("likelihood calculations with common cov look right",{
+  Bhat = rbind(c(1,2,3),c(2,4,6))
+  Shat = rbind(c(1,1,1),c(1,1,1))
+  data = set_mash_data(Bhat, Shat)
+  Ulist = cov_singletons(data)
+  mm = calc_lik_matrix(data, Ulist)
+  mm2 = calc_lik_matrix_common_cov(data, Ulist)
+  expect_equal(mm,mm2)
+}
+)
