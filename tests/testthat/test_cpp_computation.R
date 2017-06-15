036115e6495a6d5b17275d2b21518c597d8bf64d
Gao Wang <gaow@uchicago.edu>
2017-06-14 20:49:58 -0500

    Add unit test for posterior comparison

--- tests/testthat/test_cpp_computation.R
+++ tests/testthat/test_cpp_computation.R
@@ -39,3 +39,43 @@ test_that("compare likelihood computation R vs C++ in simulated data R = 1", {
   expect_equal(out1, out2, tolerance=1e-8)
 }
 )
+
+test_that("compare posterior computation R vs C++ in provided data of 100 X 5", {
+  ## Load the data.
+  load("compute_posterior_matrices_data.RData")
+  ## Compute the posterior quantities using the R implementation.
+  cat("Computing posterior matrices using R version.\n")
+  out.time <- system.time(out.mem <- profmem::profmem({
+    out1 <- compute_posterior_matrices(data,Ulist,posterior_weights,
+                                       algorithm.version = "R")
+  },threshold = 1000))
+  cat(sprintf("Computation allocated %0.2f MB and took %0.2f s.\n",
+              sum(out.mem$bytes,na.rm = TRUE)/1024^2,out.time["elapsed"]))
+
+  ## Compute the posterior quantities using the Rcpp implementation.
+  cat("Computing posterior matrices using Rcpp version.\n")
+  out.time <- system.time(out.mem <- profmem::profmem({
+    out2 <- compute_posterior_matrices(data,Ulist,posterior_weights,
+                                       algorithm.version = "Rcpp")
+  },threshold = 1000))
+  cat(sprintf("Computation allocated %0.2f MB and took %0.2f s.\n",
+              sum(out.mem$bytes,na.rm = TRUE)/1024^2,out.time["elapsed"]))
+  expect_equal(out1, out2, tolerance=1e-8)
+}
+)
+
+test_that("compare posterior computation R vs C++ in simulated data R = 1", {
+  set.seed(999)
+  N = 10
+  Shat = matrix(rep(1,N),ncol=1)
+  Bhat = matrix(rnorm(N),ncol=1)
+  data = mashr::set_mash_data(Bhat,Shat)
+  Ulist = list(id=matrix(1,nrow=1))
+  posterior_weights = matrix(rep(1,N),N,1)
+  out1 <- compute_posterior_matrices(data,Ulist,posterior_weights,
+                                     algorithm.version = "R")
+  out2 <- compute_posterior_matrices(data,Ulist,posterior_weights,
+                                     algorithm.version = "Rcpp")
+  expect_equal(out1, out2, tolerance=1e-8)
+}
+)
