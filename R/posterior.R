0673ceedc70cb955c6fac8d89e6abab1706e2772
stephens999 <stephens999@gmail.com>
2017-06-15 21:30:31 -0500

    remove old functions no longer required (high memory version); and old tests

--- R/posterior.R
+++ R/posterior.R
@@ -81,60 +81,6 @@ compute_posterior_matrices <-
     stop("Algorithm version should be either \"R\" or \"Rcpp\"")
 }
 
-#' @title compute_posterior_arrays
-#' @description More detailed description of function goes here.
-#' @param data a mash data object, eg as created by \code{set_mash_data}
-#' @param Ulist list of P prior covariance matrices
-#' @return post_mean JxPxR array of posterior means
-#' @return post_mean2 JxPxR array of posterior second moments
-#' @return post_var JxPxR array of posterior variances
-# #' @return post_pos JxPxR array of posterior (marginal) probability of being positive
-#' @return post_neg JxPxR array of posterior (marginal) probability of being negative
-#' @return post_zero JxPxR array of posterior (marginal) probability of being zero
-#' @importFrom stats pnorm
-#' @export
-compute_posterior_arrays=function(data,Ulist){
-  R=n_conditions(data)
-  J=n_effects(data)
-
-  P=length(Ulist)
-  post_mean=array(NA,dim=c(J,P,R))
-  post_mean2 = array(NA,dim=c(J,P,R)) #mean squared value
-  post_zero=array(NA,dim=c(J,P,R))
-  post_neg=array(NA,dim=c(J,P,R))
-
-  for(j in 1:J){
-    bhat=as.vector(t(data$Bhat[j,]))##turn i into a R x 1 vector
-    V=get_cov(data,j)
-    Vinv <- solve(V)
-    for(p in 1:P){
-      U1 <- posterior_cov(Vinv, Ulist[[p]])
-      mu1 <- as.array(posterior_mean(bhat, Vinv, U1))
-      post_mean[j,p,]= mu1
-      post_mean2[j,p,] = mu1^2 + diag(U1) #diag(U1) is the posterior variance
-      post_var = diag(U1)
-      post_neg[j,p,] = ifelse(post_var==0,0,pnorm(0,mean=mu1,sqrt(diag(U1)),lower.tail=T))
-      post_zero[j,p,] = ifelse(post_var==0,1,0)
-    }
-  }
-  return(list(post_mean=post_mean,
-              post_zero=post_zero,
-              post_mean2= post_mean2,
-              post_neg=post_neg))
-}
-
-#' @title Compute weighted means of posterior arrays
-#' @description Generates a K x R matrix of posterior quantities (eg posterior mean) for each effect
-#' @param post_array J x K x R array of posterior quantity for each effect for each component in each condition
-#' @param posterior_weights J x K matrix of weights for each effect in each component (usually the posterior weights)
-#' @return J by R matrix of quantities (eg posterior mean) for each effect in each condition. The (j,r) element is sum_k pi[j,k] a[j,k,r]
-#' @export
-compute_weighted_quantity = function(post_array,posterior_weights){
-  R = dim(post_array)[3]
-  weighted_array = post_array * rep(posterior_weights,R)
-  return(apply(weighted_array,c(1,3),sum))
-}
-
 #' @title compute posterior probabilities
 #' @description computes posterior probabilities that each effect came from each component
 #' @param pi a K vector of mixture proportions
--- tests/testthat/test_posterior.R
+++ tests/testthat/test_posterior.R
@@ -1,39 +1,3 @@
-test_that("array of posteriors looks right", {
-  Bhat = rbind(c(1,2,3),c(2,4,6))
-  Shat = rbind(c(1,1,1),c(2,2,2))
-  data = set_mash_data(Bhat,Shat)
-  Ulist = cov_singletons(data)
-  U1 = posterior_cov(diag(3),Ulist[[1]])
-  mu1 = posterior_mean(Bhat[1,],diag(3),U1)
-  post_array_list = compute_posterior_arrays(data, Ulist)
-  expect_equal(post_array_list$post_mean[1,1,],as.vector(mu1))
-
-  U1 = posterior_cov(diag(0.5^2,3),Ulist[[2]])
-  mu1 = posterior_mean(Bhat[2,],diag(0.5^2,3),U1)
-  expect_equal(post_array_list$post_mean[2,2,],as.vector(mu1))
-  #expect_equal(post_array_list$post_pos + post_array_list$post_neg + post_array_list$post_zero, array(1,dim=c(2,3,3)))
-}
-)
-
-test_that("matrix of posterior probabilities looks right; and posterior means look right", {
-  Bhat = rbind(c(1,2,3),c(2,4,6))
-  Shat = rbind(c(1,1,1),c(2,2,2))
-  data = set_mash_data(Bhat,Shat)
-  Ulist = cov_canonical(data,c("id","sing","equal_ef","null"))
-  mm = calc_lik_matrix(data, Ulist)
-  K = length(Ulist)
-  prior = rep(1/K,K)
-  post = compute_posterior_weights(prior, mm)
-  expect_equal( cor(post[1,],mm[1,]), 1)
-  expect_equal( cor(post[2,],mm[2,]), 1)
-
-  post_array_list = compute_posterior_arrays(data, Ulist)
-  post_matrix = compute_weighted_quantity(post_array_list$post_mean,post)
-  expect_equal(rowSums(post*post_array_list$post_mean[,,1]),post_matrix[,1])
-  expect_equal(rowSums(post*post_array_list$post_mean[,,2]),post_matrix[,2])
-}
-)
-
 test_that("calculations on test set with fixg match original",{
   Bhat = rbind(c(1,2,3),c(2,4,6))
   Shat = rbind(c(1,1,1),c(2,2,2))
