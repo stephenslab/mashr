0cbbcaf9023e2ec6db81845dbd5653aa1440da59
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-06-21 23:30:04 -0500

    Profiling of memory allocations is not supported on Travis, so it is removed from testthat tests.

--- tests/testthat/test_cpp_computation.R
+++ tests/testthat/test_cpp_computation.R
@@ -7,22 +7,18 @@ test_that("compare likelihood computation R vs C++ in provided data of 100 X 5",
 
   ## Compute the likelihood matrix using the R implementation.
   cat(sprintf("Computing %d x %d likelihood matrix using R version.\n",J,P))
-  out.time <- system.time(out.mem <- profmem::profmem({
-    out1 <- calc_lik_matrix(data,Ulist,log = TRUE,algorithm.version = "R")
-  },threshold = 1000))
-  cat(sprintf(paste("Likelihood calculations allocated %0.2f MB",
-                    "and took %0.2f seconds.\n"),
-              sum(out.mem$bytes,na.rm = TRUE)/1024^2,
+  out.time <- system.time(out1 <- calc_lik_matrix(data,Ulist,log = TRUE,
+                                                  algorithm.version = "R"))
+  cat(sprintf("Likelihood calculations took %0.2f seconds.\n",
               out.time["elapsed"]))
+  
   ## Compute the likelihood matrix using the Rcpp implementation.
   cat(sprintf("Computing %d x %d likelihood matrix using Rcpp version.\n",J,P))
-  out.time <- system.time(out.mem <- profmem::profmem({
-    out2 <- calc_lik_matrix(data,Ulist,log = TRUE,algorithm.version = "Rcpp")
-  },threshold = 1000))
-  cat(sprintf(paste("Likelihood calculations allocated %0.2f MB",
-                    "and took %0.2f seconds.\n"),
-              sum(out.mem$bytes,na.rm = TRUE)/1024^2,
+  out.time <- system.time(out2 <- calc_lik_matrix(data,Ulist,log = TRUE,
+                                                  algorithm.version = "Rcpp"))
+  cat(sprintf("Likelihood calculations took %0.2f seconds.\n",
               out.time["elapsed"]))
+  
   expect_equal(out1, out2, tolerance=1e-5)
 }
 )
@@ -56,21 +52,17 @@ test_that("compare posterior computation R vs C++ in provided data of 100 X 5", 
   load("compute_posterior_matrices_data.RData")
   ## Compute the posterior quantities using the R implementation.
   cat("Computing posterior matrices using R version.\n")
-  out.time <- system.time(out.mem <- profmem::profmem({
-    out1 <- compute_posterior_matrices(data,Ulist,posterior_weights,
-                                       algorithm.version = "R")
-  },threshold = 1000))
-  cat(sprintf("Computation allocated %0.2f MB and took %0.2f s.\n",
-              sum(out.mem$bytes,na.rm = TRUE)/1024^2,out.time["elapsed"]))
+  out.time <- system.time(out1 <-
+                compute_posterior_matrices(data,Ulist,posterior_weights,
+                                           algorithm.version = "R"))
+  cat(sprintf("Computation took %0.2f seconds.\n",out.time["elapsed"]))
 
   ## Compute the posterior quantities using the Rcpp implementation.
   cat("Computing posterior matrices using Rcpp version.\n")
-  out.time <- system.time(out.mem <- profmem::profmem({
-    out2 <- compute_posterior_matrices(data,Ulist,posterior_weights,
-                                       algorithm.version = "Rcpp")
-  },threshold = 1000))
-  cat(sprintf("Computation allocated %0.2f MB and took %0.2f s.\n",
-              sum(out.mem$bytes,na.rm = TRUE)/1024^2,out.time["elapsed"]))
+  out.time <- system.time(out2 <-
+                compute_posterior_matrices(data,Ulist,posterior_weights,
+                                           algorithm.version = "Rcpp"))
+  cat(sprintf("Computation took %0.2f seconds.\n",out.time["elapsed"]))
   expect_equal(out1, out2, tolerance=1e-5)
 }
 )
