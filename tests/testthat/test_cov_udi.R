cafb045689da4bb89b8f140cd9cd74c28be60999
Gao Wang <gaow@uchicago.edu>
2017-10-31 09:23:45 -0500

    Add column / row names to simple simulation functions

--- tests/testthat/test_cov_udi.R
+++ tests/testthat/test_cov_udi.R
@@ -3,7 +3,7 @@ test_that("udi covariance computations look right", {
   Bhat = matrix(rnorm(100),ncol=5)
   L = matrix(rnorm(25),nrow=5,ncol=5)
   V = L %*% t(L)
-  data = set_mash_data(Bhat,1,V)
+  data = set_mash_data(Bhat,1,V=V)
   expect_error(cov_udi(data, c("U","U","D","D","A")))
   expect_error(cov_udi(data, c("U","U","I","I","I")))
   expect_equal(names(cov_udi(data, c("U","U","I","I","D"))), "cov_udi_UUIID")
