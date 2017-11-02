53406cbe40d29a5dc5ed5a7f384990fc99dceff6
Gao Wang <gaow@uchicago.edu>
2017-11-01 19:17:54 -0500

    Adjust likelihood & add unit tests for EE and EZ model compared with ash

--- tests/testthat/test_likelihood.R
+++ tests/testthat/test_likelihood.R
@@ -16,6 +16,7 @@ test_that("likelihood calculations on test set match original",{
   Bhat = rbind(c(1,2,3),c(2,4,6))
   Shat = rbind(c(1,1,1),c(2,2,2))
   data = set_mash_data(Bhat,Shat)
+  data$Shat_alpha = matrix(1, nrow(data$Bhat), ncol(data$Bhat))
   Ulist = cov_canonical(data)
   m = mash(data,Ulist,grid = c(0.5,1,2),alpha=0)
   expect_equal(mash_compute_loglik(m,data),m$loglik)
