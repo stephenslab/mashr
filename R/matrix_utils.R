67d9c7af23c88270d7df56d2bf41f14783b734eb
Gao Wang <gaow@uchicago.edu>
2018-09-21 00:52:32 -0500

    Use a customized labelled stop function for parameter check

--- R/matrix_utils.R
+++ R/matrix_utils.R
@@ -1,23 +1,25 @@
 ## @title A wrapper function to `stop` call
-check_failed = function(x, msg) stop(paste(deparse(substitute(x)), msg), call.=F)
+labelled_stop = function(x, msg) 
+    stop(paste(gsub("\\s+", " ", paste0(deparse(x))), msg), call.=F)
 
 ## @title Basic sanity check for covariance matrices
 ## @param X input matrix
 check_covmat_basics = function(x) {
-    if (!is.matrix(x))
-        check_failed(x, "is not a matrix")
-    if (!is.numeric(x))
-        check_failed(x, "is not a numeric matrix")
-    if (any(is.na(x)))
-        check_failed(x, "cannot contain NA values")
-    if (any(is.infinite(x)))
-        check_failed(x, "cannot contain Inf values")
-    if (any(is.nan(x)))
-        check_failed(x, "cannot contain NaN values")
-    if (nrow(x) != ncol(x))
-        check_failed(x, "is not a square matrix")
-    if (!isSymmetric(x))
-        check_failed(x, "is not a symmetric matrix")
+  label = substitute(x)
+  if (!is.matrix(x))
+    labelled_stop(label, "is not a matrix")
+  if (!is.numeric(x))
+    labelled_stop(label, "is not a numeric matrix")
+  if (any(is.na(x)))
+    labelled_stop(label, "cannot contain NA values")
+  if (any(is.infinite(x)))
+    labelled_stop(label, "cannot contain Inf values")
+  if (any(is.nan(x)))
+    labelled_stop(label, "cannot contain NaN values")
+  if (nrow(x) != ncol(x))
+    labelled_stop(label, "is not a square matrix")
+  if (!isSymmetric(x))
+    labelled_stop(label, "is not a symmetric matrix")
   return(TRUE)
 }
 
@@ -25,6 +27,6 @@ check_covmat_basics = function(x) {
 ## @param X input matrix
 check_positive_definite = function(x) {
   check_covmat_basics(x)
-  tryCatch(chol(x), error = function(e) check_failed(x, "must be positive definite"))
+  tryCatch(chol(x), error = function(e) labelled_stop(substitute(x), "must be positive definite"))
   return(TRUE)
 }
