1d06a97351b5592585b113856c69910e97bf6c34
zouyuxin <yuxin.zou.stat@gmail.com>
2018-11-23 00:41:16 -0600

    Update unit test

--- inst/code/get_est_null_cor.R
+++ inst/code/get_est_null_cor.R
@@ -14,6 +14,6 @@ simdata = simple_sims(500,5,0.5)
 data = mash_set_data(simdata$Bhat, simdata$Shat, alpha=1)
 U.c = cov_canonical(data)
 
-V.est = estimate_null_correlation(data, U.c, max_iter = 3, track_fit = TRUE, algorithm.version = 'R')
+V.est = estimate_null_correlation(data, U.c, max_iter = 3, track_fit = TRUE)
 
 saveRDS(V.est, 'tests/testthat/estimate_null_cor_alpha.rds')
