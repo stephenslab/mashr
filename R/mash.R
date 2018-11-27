e2e678e47d8a7b9416b2a17fd39ebd7c1e544ed3
Gao Wang <gaow@uchicago.edu>
2018-11-27 16:28:34 -0600

    Add posterior weights to default output

--- R/mash.R
+++ R/mash.R
@@ -17,7 +17,7 @@
 #' @param A the linear transformation matrix, Q x R matrix. This is used to compute the posterior for Ab.
 #' @param posterior_samples the number of samples to be drawn from the posterior distribution of each effect.
 #' @param seed A random number seed to use when sampling from the posteriors. It is used when \code{posterior_samples > 0}.
-#' @param outputlevel controls amount of computation / output; 1: output only estimated mixture component proportions, 2: and posterior estimates, 3: and posterior covariance matrices, 4: and likelihood matrices and posterior weights
+#' @param outputlevel controls amount of computation / output; 1: output only estimated mixture component proportions, 2: and posterior estimates, 3: and posterior covariance matrices, 4: and likelihood matrices
 #' @return a list with elements result, loglik and fitted_g
 #' @examples
 #' Bhat = matrix(rnorm(100),ncol=5) # create some simulated data
@@ -181,14 +181,16 @@ mash = function(data,
   }
   # results
   fitted_g = list(pi=pi_s, Ulist=Ulist, grid=grid, usepointmass=usepointmass)
+  names(posterior_weights) = which(which.comp)
   m=list(result = posterior_matrices,
          loglik = loglik, vloglik = vloglik,
          null_loglik = null_loglik,
          alt_loglik = alt_loglik,
-         fitted_g = fitted_g, alpha=data$alpha)
+         fitted_g = fitted_g,
+         posterior_weights = posterior_weights,
+         alpha=data$alpha)
   #for debugging
-  names(posterior_weights) = which(which.comp)
-  if(outputlevel==4){m = c(m,list(lm=lm,posterior_weights=posterior_weights))}
+  if(outputlevel==4){m = c(m,list(lm=lm))}
   class(m) = "mash"
   return(m)
 }
@@ -228,7 +230,8 @@ mash_compute_posterior_matrices = function(g, data, pi_thresh = 1e-10, algorithm
                                                   posterior_weights,
                                                   algorithm.version, A=A, output_posterior_cov=output_posterior_cov,
                                                   posterior_samples = posterior_samples, seed=seed)
-  return(posterior_matrices)
+  names(posterior_weights) = which(which.comp)
+  return(list(result=posterior_matrices, posterior_weights = posterior_weights))
 }
 
 
