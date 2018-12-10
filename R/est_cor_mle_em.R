82316b13543e8ab501d0a9bbe86d6c62dad5ee42
zouyuxin <yuxin.zou.stat@gmail.com>
2018-12-10 12:18:26 -0600

    Add gradient for 2x2 case

--- R/est_cor_mle_em.R
+++ R/est_cor_mle_em.R
@@ -1,5 +1,5 @@
 #' @export
-estimate_null_correlation_mle_em = function(data, Ulist, init, max_iter = 10, tol=1e-4,
+estimate_null_correlation_mle_em = function(data, Ulist, init, max_iter = 10, tol=1e-4, grad = TRUE,
                                             track_fit=FALSE, prior = c('nullbiased', 'uniform')){
   if(class(data) != 'mash'){
     stop('data is not a "mash" object')
@@ -42,7 +42,17 @@ estimate_null_correlation_mle_em = function(data, Ulist, init, max_iter = 10, to
 
     # max_V
     theta = cor_theta(V)
-    opt = nlminb(theta, neglogLik_m, input = lump)
+    if(grad){
+      if(R!=2){
+        stop('Gradient only available for R=2')
+      }
+      print('Using gradient')
+      opt = optim(theta, neglogLik_m, gr = gradient, input = lump)
+    }else{
+      print('No gradient')
+      opt = nlminb(theta, neglogLik_m, input = lump)
+    }
+    opt = nlminb(theta, neglogLik_m, gradient = gradient, input = lump)
     V = theta_cor(opt$par, R)
 
     # log_liks = c(log_liks, -opt$objective)
@@ -79,6 +89,20 @@ neglogLik_m = function(theta, input){
   Eb = E_V(data, model)
 
   0.5*log(det(V))*J + 0.5*tr(Vinv %*% Eb)
+}
+
+gradient = function(theta, input){
+  data = input$data
+  model = input$model
+  l = pi*exp(theta)/(exp(theta)+1)
+  cosl = cos(l)
+  sinl = sin(l)
+  cotl = cosl/sinl
+  cscl = 1/sinl
+  Eb = E_V(data, model)
+  dump = (exp(theta) + 1)^2
+
+  pi*cotl/(dump) - pi*cotl*cscl^2/(dump) * (Eb[1,1]+Eb[2,2]) + pi*cscl*(cotl^2 + cscl^2)/dump
 }
 
 # E_V = function(data, m.model){
