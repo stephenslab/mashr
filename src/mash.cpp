bcd18379857e261502a1590a5b54c86952162c2b
Gao Wang <gaow@uchicago.edu>
2019-04-02 13:36:43 -0500

    Add interface for posterior computation known V

--- src/mash.cpp
+++ src/mash.cpp
@@ -133,3 +133,54 @@ Rcpp::List calc_post_rcpp(Rcpp::NumericMatrix b_mat,
 			Rcpp::Named("post_neg") = pc.NegativeProb());
 	}
 }
+
+
+// [[Rcpp::export]]
+// This only works for multivariate case (U is 3D array)
+Rcpp::List calc_post_v_known_rcpp(Rcpp::NumericMatrix b_mat,
+                          Rcpp::NumericMatrix a_mat,
+                          Rcpp::NumericVector V_3d,
+                          Rcpp::NumericVector Vinv_3d,
+                          Rcpp::NumericVector U_3d,
+                          Rcpp::NumericMatrix posterior_weights,
+                          int report_type)
+{
+
+	// hide armadillo warning / error messages
+	// std::ostream nullstream(0);
+	// arma::set_stream_err2(nullstream);
+	// set cube data from R 3D array
+	Rcpp::IntegerVector dimU = U_3d.attr("dim");
+	arma::cube U_cube(U_3d.begin(), dimU[0], dimU[1], dimU[2]);
+	Rcpp::IntegerVector dimV = V_3d.attr("dim");
+	if (dimV.size() == 3) {
+		arma::cube V_cube(V_3d.begin(), dimV[0], dimV[1], dimV[2]);
+		arma::cube Vinv_cube(V_3d.begin(), dimV[0], dimV[1], dimV[2]);
+		PosteriorMASH_V_known pc(Rcpp::as<arma::mat>(b_mat),
+		                 Rcpp::as<arma::mat>(a_mat),
+		                 V_cube, Vinv_cube,
+		                 U_cube);
+		pc.compute_posterior(Rcpp::as<arma::mat>(posterior_weights), report_type);
+		return Rcpp::List::create(
+			Rcpp::Named("post_mean") = pc.PosteriorMean(),
+			Rcpp::Named("post_sd") = pc.PosteriorSD(),
+			Rcpp::Named("post_cov") = pc.PosteriorCov(),
+			Rcpp::Named("post_zero") = pc.ZeroProb(),
+			Rcpp::Named("post_neg") = pc.NegativeProb());
+	} else {
+		// common V
+		arma::mat V_mat(V_3d.begin(), dimV[0], dimV[1]);
+		arma::mat Vinv_mat(Vinv_3d.begin(), dimV[0], dimV[1]);
+		PosteriorMASH_V_known pc(Rcpp::as<arma::mat>(b_mat),
+		                 Rcpp::as<arma::mat>(a_mat),
+		                 V_mat, Vinv_mat,
+		                 U_cube);
+		pc.compute_posterior_comcov(Rcpp::as<arma::mat>(posterior_weights), report_type);
+		return Rcpp::List::create(
+			Rcpp::Named("post_mean") = pc.PosteriorMean(),
+			Rcpp::Named("post_sd") = pc.PosteriorSD(),
+			Rcpp::Named("post_cov") = pc.PosteriorCov(),
+			Rcpp::Named("post_zero") = pc.ZeroProb(),
+			Rcpp::Named("post_neg") = pc.NegativeProb());
+	}
+}
