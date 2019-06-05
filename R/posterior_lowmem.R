d41b4391baf2d7b10d5074558fe81c8d28e7f993
zouyuxin <yuxin.zou.stat@gmail.com>
2019-06-05 15:59:59 -0500

    Force Posterior Samples to be 3 dim array

--- R/posterior_lowmem.R
+++ R/posterior_lowmem.R
@@ -123,13 +123,10 @@ compute_posterior_matrices_general_R=function(data,A,Ulist,posterior_weights,out
     posterior_matrices$PosteriorCov = res_post_cov
   }
   if(posterior_samples > 0){
-    res_post_samples = abind(res_post_samples, along = 0) # dim J x M x Q; dim is J x M if Q = 1
-    if(length(dim(res_post_samples)) == 3){
-      res_post_samples = aperm(res_post_samples, c(1,3,2)) # dim J x Q x M
-      dimnames(res_post_samples) <- list(rownames(data$Bhat), row.names(A), paste0("sample_",(1:posterior_samples)))
-    }else{
-      dimnames(res_post_samples) <- list(rownames(data$Bhat), paste0("sample_",(1:posterior_samples)))
-    }
+    res_post_samples = abind(res_post_samples, along = 0,force.array=TRUE) # dim J x M x Q; dim is J x M if Q = 1
+    res_post_samples = array(res_post_samples, dim=c(J,posterior_samples,Q))
+    res_post_samples = aperm(res_post_samples, c(1,3,2)) # dim J x Q x M
+    dimnames(res_post_samples) <- list(rownames(data$Bhat), row.names(A), paste0("sample_",(1:posterior_samples)))
 
     posterior_matrices$PosteriorSamples = res_post_samples
   }
--- tests/testthat/test_sampling.R
+++ tests/testthat/test_sampling.R
@@ -11,13 +11,13 @@ test_that("Samples from the posterior look right", {
                res$PosteriorMean,tol = res$PosteriorSD)
 })
 
-test_that("Samples from the posterior look right", {
+test_that("Samples from the posterior with linear transformation look right", {
   set.seed(100)
   test = simple_sims()
   data = mash_set_data(test$Bhat, test$Shat)
   U = cov_canonical(data)
   res = mash(data,U, algorithm.version = 'R', posterior_samples = 100, verbose = F, A = matrix(c(1,1,0,0,0), 1, 5))$result
-  expect_equal(dim(res$PosteriorSamples), c(400,100))
-  expect_equal(as.matrix(apply(res$PosteriorSamples, 1, mean)),
+  expect_equal(dim(res$PosteriorSamples), c(400,1,100))
+  expect_equal(as.matrix(apply(res$PosteriorSamples, 1, rowMeans)),
                res$PosteriorMean,tol = res$PosteriorSD)
 })
