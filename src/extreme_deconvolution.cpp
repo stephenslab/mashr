8979d56dea9590f48177b1433223b579d0f1b485
Gao Wang <wangow@gmail.com>
2020-05-16 14:44:39 -0400

    Merge C code to one header file

--- src/extreme_deconvolution.cpp
+++ src/extreme_deconvolution.cpp
@@ -8,7 +8,7 @@ extern "C" {
 #include <time.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
-#include "proj_gauss_mixtures.h"
+#include "extreme_deconvolution.h"
 }
 // [[Rcpp::depends(RcppGSL)]]
 
